2bc5b1b856476792ce052e2c05c0b18ac64e1887
nicolastarzia <git@nicolastarzia.com>
2016-08-29 22:12:46 -0300

    Hello World!

---
+++ opmlLoader.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gilliek/go-opml/opml"
+)
+
+func requestHTTP(outlin opml.Outline, bufWRT *bufio.Writer) {
+	currentLink := outlin.XMLURL
+	resp, err := http.Get(currentLink)
+	if err == nil && resp.StatusCode == 200 {
+		bufWRT.WriteString("<outline type=\"rss\" text=\"" + outlin.Text + "\" title=\"" + outlin.Title + "\" htmlUrl=\"" + outlin.HTMLURL + "\" xmlUrl=\"" + outlin.XMLURL + "\"/>\n")
+		fmt.Printf("%q - %q\n", currentLink, resp.Status)
+	} else {
+		fmt.Printf("%q - %q\n", currentLink, " inválido")
+	}
+	bufWRT.Flush()
+}
+
+func recursiveOutline(outlin opml.Outline, bufWRT *bufio.Writer) {
+	currentLink := outlin.XMLURL
+	if outlin.Type == "rss" && len(currentLink) > 0 {
+		requestHTTP(outlin, bufWRT)
+	} else {
+		bufWRT.WriteString("<outline text=\"" + outlin.Text + "\" title=\"" + outlin.Title + "\">\n")
+	}
+	if outlin.Outlines != nil {
+		for myOuts := range outlin.Outlines {
+			recursiveOutline(outlin.Outlines[myOuts], bufWRT)
+		}
+		bufWRT.WriteString("</outline>\n")
+	}
+}
+
+func main() {
+	filename := "feedly.opml"
+	fmt.Printf("Filename: %s\n", filename)
+	myFile, err := filepath.Abs(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc, err := opml.NewOPMLFromFile(myFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fil, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w := bufio.NewWriter(fil)
+	w.WriteString(`<?xml version="1.0" encoding="UTF-8"?>
+
+<opml version="1.0">
+    <head>
+        <title>Nicolas subscriptions in feedly Cloud</title>
+    </head>
+    <body>
+    `)
+	for outlin := range doc.Body.Outlines {
+		recursiveOutline(doc.Body.Outlines[outlin], w)
+	}
+	w.WriteString(`
+    </body>
+    </opml>`)
+	w.Flush()
+	fmt.Printf("END!!")
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# OPML Validator
+> opml, golang
+
+I have a big OPML file, and sometimes I need to shrink and remove older RSS's
+
+## Developing
+
+To develop and run this project it's simple, put your feedly.opml file in the current folder:
+
+```shell
+git clone https://github.com/your/awesome-project.git
+cd opmlloader/
+go run .\opmlLoader.go
+```
+
+## Features
+
+What's all the bells and whistles this project can perform?
+* Read and validate all RSS
+* Request all RSS and verify if answer is "200 OK"
+* Write new OPML file
+
+## Contributing
+
+Help me to maintain this project, put your issue and send your pull request. 
+
+## Licensing
+
+"The code in this project is licensed under MIT license."
